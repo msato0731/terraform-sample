3d7c98ac99e7875be503e6b6b2b35c272ff6d5b0
msato0731 <rhcq.3127@gmail.com>
2023-07-31 15:42:50 +0900

    add: opa

--- opa-tutorial/main.tf
+++ opa-tutorial/main.tf
@@ -1,23 +1,43 @@
 provider "aws" {
-    region = "us-west-1"
+  region = "us-east-1"
 }
-resource "aws_instance" "web" {
-  instance_type = "t2.micro"
-  ami = "ami-09b4b74c"
+
+data "aws_vpc" "default" {
+  default = true
 }
-resource "aws_autoscaling_group" "my_asg" {
-  availability_zones        = ["us-west-1a"]
-  name                      = "my_asg"
-  max_size                  = 5
-  min_size                  = 1
-  health_check_grace_period = 300
-  health_check_type         = "ELB"
-  desired_capacity          = 4
-  force_delete              = true
-  launch_configuration      = "my_web_config"
+
+module "http_sg" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-security-group.git?ref=v3.10.0"
+
+  name        = "http-sg"
+  description = "Security group with HTTP ports open for everybody (IPv4 CIDR), egress ports are all world open"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 }
-resource "aws_launch_configuration" "my_web_config" {
-    name = "my_web_config"
-    image_id = "ami-09b4b74c"
-    instance_type = "t2.micro"
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
