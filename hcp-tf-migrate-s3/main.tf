3798ea872dff8cb3d800f53f67fcad6fa7c58d07
msato0731 <rhcq.3127@gmail.com>
2025-03-13 10:54:40 +0900

    HCP Terraformへのマイグレートサンプルコード作成

---
+++ hcp-tf-migrate-s3/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.90.1"
+    }
+  }
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+variable "env" {
+  description = "The environment name"
+  type        = string
+}
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "${var.env}"
+  }
+}
