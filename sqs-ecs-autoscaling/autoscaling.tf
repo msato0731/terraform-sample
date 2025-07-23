3956771cf5ca384a98757715effbcf0ceae581cf
msato0731 <rhcq.3127@gmail.com>
2025-07-23 13:50:01 +0900

    ECSオートスケーリングの最小キャパシティを0に変更し、不要なIAMロールを削除

--- sqs-ecs-autoscaling/autoscaling.tf
+++ sqs-ecs-autoscaling/autoscaling.tf
@@ -1,6 +1,6 @@
 resource "aws_appautoscaling_target" "ecs_target" {
   max_capacity       = 5
-  min_capacity       = 1
+  min_capacity       = 0
   resource_id        = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.main.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
--- sqs-ecs-autoscaling/iam.tf
+++ sqs-ecs-autoscaling/iam.tf
@@ -62,21 +62,3 @@ resource "aws_iam_role_policy" "ecs_task_sqs_policy" {
     ]
   })
 }
-
-resource "aws_iam_role" "ecs_autoscaling_role" {
-  name = "${local.prefix}-autoscaling-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "application-autoscaling.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
